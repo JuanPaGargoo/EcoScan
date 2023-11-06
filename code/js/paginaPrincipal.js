f66fadc72fdb156ddeed6ebc43e4a04ebd4bf957
Juan Pablo Garcia <juanpagargoo@gmail.com>
2023-11-05 20:23:25 -0600

    Finalizacion de la fase beta del funcionamiento de EcoScan

--- code/js/paginaPrincipal.js
+++ code/js/paginaPrincipal.js
@@ -43,3 +43,4 @@ document.addEventListener('keyup',e=>{
 })
 
 
+
--- code/js/sweetAlert.js
+++ code/js/sweetAlert.js
@@ -1,8 +1,34 @@
 function mostrarProducto(nom){
+    var product =""
+    var desc=""
+    var sust=""
+    var ima=""
+    var icono=""
+    var texto =""
+    productos.forEach(producto => {
+        const coincideConBusqueda = producto.nombre.trim().includes(nom.trim());
+        if (coincideConBusqueda) {
+            product = producto.nombre;
+            desc = producto.descripcion;
+            sust = producto.sustentable;
+            ima = producto.img;
+        }
+        console.log("Hola soy "+producto.nombre+" "+coincideConBusqueda+ " "+ nom)
+    });
+    if (sust.toLowerCase() === "si") {
+        icono = "success";
+    } else {
+        icono = "error";
+    }
+    texto = desc+'\nSustentable: "'+sust+'".'
     Swal.fire({
-        icon:"success",
-        title:"Producto",
-        text:"descrpicion producto",
+        icon:icono,
+        title:product,
+        imageUrl:ima,
+        imageWidth:"160px",
+        imageHeight:"160px",
+        text:texto,
+        scrollbarPadding:true,
         confirmButtonText:'Cerrar',
         confirmButtonColor: "#216b5d",
         customClass:{
@@ -11,3 +37,15 @@ function mostrarProducto(nom){
         }   
     });   
 }
+
+const botones = document.querySelectorAll('.product-b');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+    const nombre = boton.textContent;
+    mostrarProducto(nombre)
+    });
+});
+
+
+
