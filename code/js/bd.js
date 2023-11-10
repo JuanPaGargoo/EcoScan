462a8d37020abaedf08bbf59ef07d061122e1606
Juan Pablo Garcia <juanpagargoo@gmail.com>
2023-11-10 10:16:28 -0600

    Quedo lista la pagina para presentarla

--- code/js/bd.js
+++ code/js/bd.js
@@ -40,7 +40,64 @@ var productos =[
     descripcion:"Formula sintética pero proveniente del medio ambiente; tiene programas de cuidado del medio ambiente; esta en la lista de PETA de libre de crueldad animal; utiliza materiales recliclables; 100% vegana; utiliza energías renovables para la fabricación de productos.",
     img:"../img/productos/GentleMoistruriserEthicalBeauty.png"},
 
-    /*11*/{ nombre:"",sustentable:"",
+    /*11*/{ nombre:"Aceite Luminoso (AGUA DE NUBE)",sustentable:"Si",
+    descripcion:"Producto elaborado 100% con ingredientes orgánicos certificados, 100% vegano, elaborado a mano, con ingredientes de comercio justo, biodegradable y eco-amigable. Certificado por PETA en el programa “BEAUTY WITHOUT BUNNIES”, en la categoría “CRUELTY-FREE AND VEGAN”. ",
+    img:"../img/productos/aceiteLuminoso.png"},  
+
+    /*12*/{ nombre:"Aloe Vera 100% Puro (AGUA DE NUBE)",sustentable:"Si",
+    descripcion:"Producto elaborado 100% con ingredientes orgánicos certificados, 100% vegano, elaborado a mano, con ingredientes de comercio justo, biodegradable y eco-amigable. Certificado por PETA en el programa “BEAUTY WITHOUT BUNNIES”, en la categoría “CRUELTY-FREE AND VEGAN”. ",
+    img:"../img/productos/aloeVera.png"},      
+
+    /*13*/{ nombre:"Agua de Sal Rosa del Himalaya e Incienso (AGUA DE NUBE)",sustentable:"Si",
+    descripcion:"Producto elaborado 100% con ingredientes orgánicos certificados, 100% vegano, elaborado a mano, con ingredientes de comercio justo, biodegradable y eco-amigable. Certificado por PETA en el programa “BEAUTY WITHOUT BUNNIES”, en la categoría “CRUELTY-FREE AND VEGAN”. ",
+    img:"../img/productos/aguaDeSalRosa.png"},      
+
+    /*14*/{ nombre:"Balsamo de Karite y Calendula (AGUA DE NUBE)",sustentable:"Si",
+    descripcion:"Producto elaborado 100% con ingredientes orgánicos certificados, 100% vegano, elaborado a mano, con ingredientes de comercio justo, biodegradable y eco-amigable. Certificado por PETA en el programa “BEAUTY WITHOUT BUNNIES”, en la categoría “CRUELTY-FREE AND VEGAN”. ",
+    img:"../img/productos/balsamoDeKarite.png"},      
+    
+
+    /*15*/{ nombre:"Ultra-D Protector Solar FPS 40 (THE CHEMIST LOOK)",sustentable:"Si",
+    descripcion:"El protector solar ULTRA-D de amplio espectro (FPS 40 - UVB + UVA) y larga duración, es una auténtica defensa para la piel. Su textura es liviana y de rápida absorción. Con filtros químicos y físicos. Aporta antioxidantes, humecta e hidrata la piel de forma inmediata y prolongada. Aumenta los niveles de Vitamina D en la piel. Apto para todo tipo de pieles.",
+    img:"../img/productos/ultraDProtectorSolar.png"},      
+
+    /*16*/{ nombre:"Palmolive Naturals Mandarina y Romero Jabón en Barra (Palmolive®)",sustentable:"No",
+    descripcion:"Este jabón para el cuerpo tiene una fórmula nutritiva enriquecida con ingredientes 100% de origen natural y una deliciosa fragancia con extractos de mandarina y romero que revitaliza tu piel. Los centros de producción de Colgate-Palmolive se encuentran entre los 10 primeros centros de produccion que mas contaminan.",
+    img:"../img/productos/palmoliveNatural.png"},      
+
+    /*17*/{ nombre:"Colgate Natural Extracts Frutos Rojos y Açaí (Colgate®)",sustentable:"No",
+    descripcion:"Cuida naturalmente tu boca con la nueva pasta de dientes Colgate® Natural Extracts Frutos Rojos y Açaí con aceite antioxidante*. Esta pasta dental con flúor es libre de gluten. Los centros de producción de Colgate-Palmolive se encuentran entre los 10 primeros centros de produccion que mas contaminan.",
+    img:"../img/productos/colgateNaturalExtracts.png"}, 
+    
+    /*18*/{ nombre:"Gillette MACH3 Turbo máquina para afeitar recargable (Gillette)",sustentable:"No",
+    descripcion:"Las afeitadoras Gillette MACH3 cuentan con hojas más fuertes que el acero, y duran hasta hasta 2 veces más que una afeitadora desechable. Está claro que cumplen su función a la perfección, pero no se debe olvidar que buena parte de su estructura está fabricada a base de plástico y supone unag gran fuente de contaminación.",
+    img:"../img/productos/gilletteMarch3Turbo.png"},
+
+    /*19*/{ nombre:"Barra Antitranspirante Ocean Legend (Old Spice)",sustentable:"No",
+    descripcion:"El antitranspirante en barra Old Spice Ocean Legend hará que te sientas legendario y estes protegido durante todo el día gracias a su poderosa fragancia masculina cítrica, con matices de madera y notas acuáticas con su tecnología 4X Active Defense*. Los desodorantes en aerosol todavía llevan propelentes de gas que contribuyen con la contaminación del aire y el cambio climático.",
+    img:"../img/productos/antitranspiranteOceanLegend.png"},
+    
+    /*20*/{ nombre:"Crema Para Peinar Restauración (Pantene)",sustentable:"No",
+    descripcion:"Experimenta más control* de tu cabello, desde la mañana hasta la noche, con la Crema para Peinar Repara de Pantene, la cual repara las puntas, donde normalmente el cabello suele estar más seco. Está vinculada a la deforestación de bosques tropicales en la isla de Sumatra (Indonesia).",
+    img:"../img/productos/cremaParaPeinarPantene.png"},     
+
+    /*21*/{ nombre:"Enjuague Bucal 3DWhite Brilliant Fresh (Oral-B)",sustentable:"No",
+    descripcion:"El enjuague bucal Oral-B 3DWhite elimina manchas superficiales y bacterias que causan el mal aliento. Puede producir daños en el aparato digestivo de los animales marinos que los ingieren. Los aditivos que llevan estos microplásticos pueden además provocar alteraciones en el sistema neurológico.",
+    img:"../img/productos/EnjuagueBucal3DOralB.png"}, 
+
+    /*22*/{ nombre:"Desodorate En Aerosol AXE Black BZRP (AXE)",sustentable:"No",
+    descripcion:"Su fragancia de alta definición, máxima duración y frescura, crea un irresistible e intenso aroma a pera frozen y cedro que dura todo el día. Los desodorantes en aerosol todavía llevan propelentes de gas que contribuyen con la contaminación del aire y el cambio climático. ",
+    img:"../img/productos/axeBZRPpng.png"},      
+
+    /*23*/{ nombre:"Acondicionador Nutritive Secrets Ritual de Reparación (Dove)",sustentable:"No",
+    descripcion:"El Acondicionador Dove Ritual de Reparación te ayuda a tener un pelo más fuerte y resistente al daño. Greenpeace denuncia que la multinacional Unilever, propietaria de algunas de las marcas y productos más famosos en el mercado, como los cosméticos Dove, está contribuyendo a la destrucción de los últimos bosques donde vive el orangután y otras especies amenazadas, provocando el incremento de la emisión de gases de efecto invernadero responsables del cambio climático.",
+    img:"../img/productos/AcondicionadorNutrutiveDove.png"},      
+
+    /*24*/{ nombre:"Agua Micelar Con Agua De Rosas (Nivea)",sustentable:"No",
+    descripcion:"Elimina el maquillaje a prueba de agua, Tonifica y purifica la piel sin secarla, Limpia y refresca la piel. Los centros de producción de P&G se encuentran entre los 10 primeros centros de produccion que mas contaminan. ",
+    img:"../img/productos/aguaMicelarConAguaDeRosasNivea.png"}, 
+    
+    /*25*/{ nombre:"",sustentable:"",
     descripcion:"",
     img:""},      
 
---
+++ code/js/recomendaciones.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.button')
+const nav = document.querySelector('.nav')
+const divLogo = document.querySelector('.div-logo')
+const divProd = document.querySelector('.div-products')
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('activo')
+    
+})
